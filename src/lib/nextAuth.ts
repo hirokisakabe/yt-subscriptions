a2d4be67f7dd77b83e89225ba4852ca4ceed1ddd
Hiroki SAKABE <hiroki.sakabe@icloud.com>
2023-10-11 13:42:10 +0900

    アクセストークンのリフレッシュ処理を追加

--- src/lib/nextAuth.ts
+++ src/lib/nextAuth.ts
@@ -1,7 +1,7 @@
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
-import { AuthOptions } from "next-auth";
+import { AuthOptions, TokenSet } from "next-auth";
 
 if (!process.env.GOOGLE_CLIENT_ID) {
   throw new Error("Missing GOOGLE_CLIENT_ID");
@@ -19,6 +19,9 @@ const authOptions: AuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       authorization: {
         params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
           scope:
             "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/youtube.readonly",
         },
@@ -27,7 +30,7 @@ const authOptions: AuthOptions = {
   ],
   adapter: PrismaAdapter(prisma),
   callbacks: {
-    session: ({ session, user }: any) => {
+    async session({ session, user }: any) {
       if (user) {
         session.user.id = user.id;
         session.user.name = user.name;
@@ -35,9 +38,58 @@ const authOptions: AuthOptions = {
         session.user.image = user.picture;
       }
 
+      const [google] = await prisma.account.findMany({
+        where: { userId: user.id, provider: "google" },
+      });
+      if (google.expires_at && google.expires_at * 1000 < Date.now()) {
+        // If the access token has expired, try to refresh it
+        try {
+          // https://accounts.google.com/.well-known/openid-configuration
+          // We need the `token_endpoint`.
+          const response = await fetch("https://oauth2.googleapis.com/token", {
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams({
+              client_id: process.env.GOOGLE_CLIENT_ID || "",
+              client_secret: process.env.GOOGLE_CLIENT_SECRET || "",
+              grant_type: "refresh_token",
+              refresh_token: google.refresh_token || "",
+            }),
+            method: "POST",
+          });
+
+          const tokens: TokenSet = await response.json();
+
+          if (!response.ok) throw tokens;
+
+          await prisma.account.update({
+            data: {
+              access_token: tokens.access_token,
+              // @ts-ignore
+              expires_at: Math.floor(Date.now() / 1000 + tokens.expires_in),
+              refresh_token: tokens.refresh_token ?? google.refresh_token,
+            },
+            where: {
+              provider_providerAccountId: {
+                provider: "google",
+                providerAccountId: google.providerAccountId,
+              },
+            },
+          });
+        } catch (error) {
+          console.error("Error refreshing access token", error);
+          // The error property will be used client-side to handle the refresh token error
+          session.error = "RefreshAccessTokenError";
+        }
+      }
       return session;
     },
   },
 };
 
 export const nextAuth = { authOptions };
+
+declare module "next-auth" {
+  interface Session {
+    error?: "RefreshAccessTokenError";
+  }
+}
